82eddd4e4fcada6945c49dd105fde8b17c1fcbd9
weigao <weigao@hotmail.com>
2014-05-22 16:31:56 -0700

    Candy problem.

---
+++ src/test/java/CandyTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CandyTest {
+  private Candy solution;
+
+  @Before
+  public void init() {
+    solution = new Candy();
+  }
+
+  @Test
+  public void test1() {
+    int[] ratings = { 2, 1 };
+    int candies = solution.candy(ratings);
+    assertEquals(3, candies);
+  }
+
+  @Test
+  public void test2() {
+    int[] ratings = { 1, 2, 2 };
+    int candies = solution.candy(ratings);
+    assertEquals(4, candies);
+  }
+}
